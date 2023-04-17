f5e5c07ad84d1ed3753d925aa97d94c9e164cdca
fregnani123 <fabianofregnani@hotmail.com>
2023-04-17 17:33:29 -0300

    estudos

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 
-const fs = require('fs').promises;
-const path = require('path');
-const  caminhoArquivo =  path.resolve(__dirname,'.', 'teste.txt');
 
-fs.writeFile(caminhoArquivo, 'Frase 1 ', {flag: 'w'} );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs').promises;
+// const path = require('path');
+// const  caminhoArquivo =  path.resolve(__dirname,'.', 'teste.txt');
+
+// fs.writeFile(caminhoArquivo, 'Frase 1 ', {flag: 'w'} );
+
+
+// let n = [5,6,8,7,4,52,61,40,]
+
+// for(numeros of n){
+//  if(numeros % 2 === 0){
+//     console.log('par')
+//     }else{console.log('impar')}
+//  }
