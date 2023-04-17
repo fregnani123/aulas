ce187dc7c753dbba00ad03222162b6d6f9fe335d
fregnani123 <fabianofregnani@hotmail.com>
2023-04-17 17:43:37 -0300

    estudos

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
+const express = require('express')
+const app = express();
 
+const PORT = 3000
 
-
-
+app.get('/', (req,res) => {
+   res.send('Hello world')
+}).listen(PORT, (err)=>{
+  if(err) {
+   console.log(err)
+  }else{
+   console.log('Servidor rodando na ' + PORT)
+  }
+})
 
 
 
