3c582484dfea55d0208dc4ddad2859955a94754c
fregnani123 <fabianofregnani@hotmail.com>
2023-04-24 16:58:40 -0300

    atualizar estudos

--- treino.js
+++ treino.js
@@ -5,6 +5,10 @@ for(let i = 0; i < 10 ; ++i){
 }
 let n = ('063.912.989-71')
 
-let novoN= n.replace(/\D+/g,'')
+let novoN= n.replace(/\D+/g,'');
+let novoCPFSemDigito = novoN.slice(2)
+let multCPF = novoCPFSemDigito.length + 1
+
 
 console.log(novoN)
+console.log( multCPF)
