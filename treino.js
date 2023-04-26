2c82304ae24f497a0e3aefcef7fdf383f98f7586
fregnani123 <fabianofregnani@hotmail.com>
2023-04-26 19:07:40 -0300

    atualizar estudos

--- treino.js
+++ treino.js
@@ -5,6 +5,9 @@ let n = ('063.912.989-71')
 
 let novoN= n.replace(/\D+/g,'');
 let novoCPFSemDigito = novoN.slice(0,-2)
+
+
+function geraDigito(novoCPFSemDigito){
 let reverso = novoCPFSemDigito.length + 1
 total = 0 
 for(let stringNumerica of novoCPFSemDigito){
@@ -14,4 +17,10 @@ total += reverso * Number(stringNumerica)
    
 }
 let digito = 11 - (total % 11);
-console.log(digito)
+return digito <= 9 ? String(digito) : "0";
+}
+
+let digito1 = geraDigito(novoCPFSemDigito)
+let digito2 = geraDigito(novoCPFSemDigito + digito1)
+
+console.log(`${novoCPFSemDigito}${digito1}${digito2}`)
