e813b4d3a0a48bb00c2eb4eaa3ebb691c11b20fc
fregnani123 <fabianofregnani@hotmail.com>
2023-04-27 17:23:27 -0300

    atualizar

--- treino.js
+++ treino.js
@@ -1,12 +1,27 @@
 
 const args = process.argv.slice(2)
 
-const a = Number(args[0]);
-const b = Number(args[1]);
+const a = Number(args[1]);
+const b = Number(args[2]);
+let c = 0
 
-const soma = a + b 
-console.log(soma)
-
+function soma(){
+if (args[0] === 's' ){
+  c = a + b;
+  console.log(c)
+}else if (args[0] === 'd') {
+   if (b === 0) {
+     console.log('Não é possível dividir por zero.');
+   } else {
+     c = a / b;
+     console.log(c);
+   }
+ } else {
+   console.log('Operação inválida.');
+ }
+  
+}
+soma()
 
 
 
