992e54689bbc449429e86b34ff228f80fde60bab
fregnani123 <fabianofregnani@hotmail.com>
2023-04-27 17:05:22 -0300

    estudos

--- treino.js
+++ treino.js
@@ -1,26 +1,39 @@
 
+const args = process.argv.slice(2)
+
+const a = Number(args[0]);
+const b = Number(args[1]);
+
+const soma = a + b 
+console.log(soma)
 
 
-let n = ('063.912.989-71')
-
-let novoN= n.replace(/\D+/g,'');
-let novoCPFSemDigito = novoN.slice(0,-2)
 
 
-function geraDigito(novoCPFSemDigito){
-let reverso = novoCPFSemDigito.length + 1
-total = 0 
-for(let stringNumerica of novoCPFSemDigito){
+
+
+
+
+// let n = ('063.912.989-71')
+
+// let novoN= n.replace(/\D+/g,'');
+// let novoCPFSemDigito = novoN.slice(0,-2)
+
+
+// function geraDigito(novoCPFSemDigito){
+// let reverso = novoCPFSemDigito.length + 1
+// total = 0 
+// for(let stringNumerica of novoCPFSemDigito){
    
-total += reverso * Number(stringNumerica)
-   reverso--
+// total += reverso * Number(stringNumerica)
+//    reverso--
    
-}
-let digito = 11 - (total % 11);
-return digito <= 9 ? String(digito) : "0";
-}
+// }
+// let digito = 11 - (total % 11);
+// return digito <= 9 ? String(digito) : "0";
+// }
 
-let digito1 = geraDigito(novoCPFSemDigito)
-let digito2 = geraDigito(novoCPFSemDigito + digito1)
+// let digito1 = geraDigito(novoCPFSemDigito)
+// let digito2 = geraDigito(novoCPFSemDigito + digito1)
 
-console.log(`${novoCPFSemDigito}${digito1}${digito2}`)
+// console.log(`${novoCPFSemDigito}${digito1}${digito2}`)
