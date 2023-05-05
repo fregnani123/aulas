e70e67605f9838bb2a5e7e73348e59d6ff2ad3c0
fregnani123 <fabianofregnani@hotmail.com>
2023-05-04 22:23:30 -0300

    atualizar

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.set('views', path.resolve(__dirname, 'views' ));
+app.use(express.static(path.resolve(__dirname, 'public')))
 app.set('view engine', 'ejs');
 
 app.use(routes);
